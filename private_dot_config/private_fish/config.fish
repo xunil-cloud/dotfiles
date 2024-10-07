953f6e200374076b66aa5559370eb869c20924cf
Benson Chen <bensonchen@aten.com.tw>
2024-10-07 11:59:17 +0800

    fish: update config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,5 @@
+set fish_greeting
+
 alias vi=nvim
 alias vim=nvim
 abbr che chezmoi
@@ -5,8 +7,21 @@ abbr che chezmoi
 set -x EDITOR nvim
 set -x VISUAL nvim
 
+source $HOME/.cargo/env.fish
+
+if test -d $HOME/nvim-linux64/bin
+	fish_add_path $HOME/nvim-linux/bin
+end
+
+fish_add_path $HOME/.local/bin
+
 starship init fish | source
 zoxide init fish | source
+zellij setup --generate-completion fish | source
 
-set fish_greeting
+## pyenv setting, not currently in use
 
+# set -Ux PYENV_ROOT $HOME/.pyenv
+# set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+# setup pyenv
+# pyenv init - | source
