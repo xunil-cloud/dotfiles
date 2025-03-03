ea1fa26c064baabe618ed0e5c9fafc97b7c98be4
xunil-cloud <freed602om@gmail.com>
2025-03-03 18:23:54 +0800

    nvim: comment out neo-tree plugin

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -257,16 +257,16 @@ require('lazy').setup({
     opts = {},
   },
 
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  },
+  -- {
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   branch = "v3.x",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+  --     "MunifTanjim/nui.nvim",
+  --     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  --   }
+  -- },
   {
   'stevearc/oil.nvim',
   ---@module 'oil'
@@ -693,6 +693,9 @@ cmp.setup {
   },
 }
 require("symbols-outline").setup()
+
+require('telescope').load_extension('projects')
+-- require'telescope'.extensions.projects.projects{}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
